47f6853f3234183f1ad1e53572f1a88751a63166
Melissa Perez <melissaperezg.96@gmail.com>
2020-03-19 01:46:52 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,13 +2,11 @@ class Dog
 end
 
 class Dog
-  def name=(dog_name)
-    @this_dogs_name = dog_name
-  end 
-  
+  def initialize(name)
+  @name = name 
+  end
+
   def name
-    @this_dogs_name
-  end 
-  
-  
+  @name
+  end
 end
